76c060ecaeec22637dde8ff24a926a4380c0376b
James Carlson <jxxcarlson@mac.com>
2019-04-08 15:59:37 -0400

    Updte config.exs

--- config/config.exs
+++ config/config.exs
@@ -11,12 +11,20 @@ config :log_server,
   ecto_repos: [LogServer.Repo]
 
 # Configures the endpoint
+#config :log_server, LogServerWeb.Endpoint,
+#  url: [host: "localhost"],
+#  secret_key_base: "FRhbgktc8c0f/+DpZuKkZbjXPi3GQpEZYfbxFjZt1GYP1E6bOaqgP6o3U2Q11kwh",
+#  render_errors: [view: LogServerWeb.ErrorView, accepts: ~w(html json)],
+#  pubsub: [name: LogServer.PubSub, adapter: Phoenix.PubSub.PG2],
+#  http: [port: System.get_env("PORT")]
+
+
 config :log_server, LogServerWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "FRhbgktc8c0f/+DpZuKkZbjXPi3GQpEZYfbxFjZt1GYP1E6bOaqgP6o3U2Q11kwh",
-  render_errors: [view: LogServerWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: LogServer.PubSub, adapter: Phoenix.PubSub.PG2],
-  http: [port: System.get_env("PORT")]
+       http: [:inet6, port: System.get_env("PORT") || 4000],
+       url: [scheme: "https", host: "sleepy-thicket-56112.herokuapp.com", port: 443],
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+       cache_static_manifest: "priv/static/cache_manifest.json",
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configures Elixir's Logger
 config :logger, :console,
